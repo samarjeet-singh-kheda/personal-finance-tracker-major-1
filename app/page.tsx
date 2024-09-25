2deea825eda522687ffa96daba0debda77879077
samarjeet-singh-kheda <samarjeetsingh259@gmail.com>
2024-09-26 04:31:45 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <Button>Click Please</Button>
+      <Button variant="destructive">Click Please</Button>
+      <div className="bg-blue-500 text-black">Hello World!!!!</div>
+    </>
+  );
+}
