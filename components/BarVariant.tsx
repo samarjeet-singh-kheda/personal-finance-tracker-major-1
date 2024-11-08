29ded5379c66af7ba7a0b1233f1375ebb62eb26f
samarjeet-singh-kheda <samarjeetsingh259@gmail.com>
2024-11-08 14:21:36 +0530

    feat: dashboard charts (area, bar, line)

---
+++ components/BarVariant.tsx
@@ -0,0 +1,48 @@
+import { format } from "date-fns";
+import {
+  Tooltip,
+  XAxis,
+  BarChart,
+  Bar,
+  ResponsiveContainer,
+  CartesianGrid,
+} from "recharts";
+import CustomTooltip from "./CustomToolTip";
+
+type Props = {
+  data?: {
+    date: string;
+    income: number;
+    expenses: number;
+  }[];
+};
+
+function BarVariant({ data }: Props) {
+  return (
+    <ResponsiveContainer width="100%" height={350}>
+      <BarChart data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+
+        <XAxis
+          axisLine={false}
+          tickLine={false}
+          dataKey="date"
+          tickFormatter={(value) => format(value, "dd MMM")}
+          style={{ fontSize: "12px" }}
+          tickMargin={16}
+        />
+
+        <Tooltip
+          content={({ active, payload }) => (
+            <CustomTooltip active={active} payload={payload} />
+          )}
+        />
+
+        <Bar dataKey="income" fill="#3b82f6" className="drop-shadow-sm" />
+        <Bar dataKey="expenses" fill="#f43f5e" className="drop-shadow-sm" />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+}
+
+export default BarVariant;
